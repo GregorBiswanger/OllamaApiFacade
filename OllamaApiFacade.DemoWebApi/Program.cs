aabdd3c2695d4ec45e476cccc2f41cb118afca80
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2024-11-19 23:55:54 +0100

    Update NuGet packages and refactor Program.cs
    Updated project file to use newer versions of Azure.Identity, Azure.Security.KeyVault.Secrets, Microsoft.SemanticKernel, and Microsoft.SemanticKernel.Connectors.OpenAI. Removed unused using directives in Program.cs. Introduced new constants and variables for key vault and Azure OpenAI configurations. Refactored web application builder configuration to include a proxy for debugging and support for different connectors, adding Azure OpenAI chat completions for gpt-4 and gpt-35-turbo-16k models. Updated app.MapPostApiChat to include a new ModelId parameter set to "gpt-4".

--- OllamaApiFacade.DemoWebApi/Program.cs
+++ OllamaApiFacade.DemoWebApi/Program.cs
@@ -1,43 +1,38 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using OllamaApiFacade.DemoWebApi.Plugins;
-using OllamaApiFacade.DTOs;
-using OllamaApiFacade.Extensions;
-using System.Text.Json;
 using OllamaApiFacade.DemoWebApi;
+using OllamaApiFacade.Extensions;
+
+const string name = "<name>";
+const string keyVaultUrl = $"https://{name}.vault.azure.net";
+var keyVaultHelper = new KeyVaultHelper(keyVaultUrl);
+var azureOpenAiApiKey = await keyVaultHelper.GetSecretAsync("AZURE-OPENAI-API-KEY");
+var azureOpenAiEndpoint = $"https://{name}.openai.azure.com";
+const string deploymentGpt4 = "gpt-4";
+const string deploymentGpt35 = "gpt-35-turbo-16k";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureAsLocalOllamaApi();
 
-//const string name = "<NAME>";
-//const string keyVaultUrl = $"https://{name}.vault.azure.net";
-//var keyVaultHelper = new KeyVaultHelper(keyVaultUrl);
-//var azureOpenAiApiKey = await keyVaultHelper.GetSecretAsync("AZURE-OPENAI-API-KEY");
-////var openAiApiKey = await keyVaultHelper.GetSecretAsync("OPENAI-API-KEY");
-//var azureOpenAiEndpoint = $"https://{name}.openai.azure.com";
-//const string azureOpenAiDeploymentName = "gpt-4";
-
-//builder.Services.AddKernel()
-//    .AddAzureOpenAIChatCompletion(azureOpenAiDeploymentName, azureOpenAiEndpoint, azureOpenAiApiKey)
-//    .Plugins.AddFromType<TimeInformationPlugin>();
-
-//builder.Services.AddKernel().AddOpenAIChatCompletion("gpt-4o", openAiApiKey)
-//    .Plugins.AddFromType<TimeInformationPlugin>();
-
-builder.Services.AddKernel().AddLmStudio()
-    .Plugins.AddFromType<TimeInformationPlugin>();
+// With AddProxyForDebug you can use Burp Suite for communication debugging, the proxy is running on http://localhost:8080
+// Support for different connectors is also important.
+// In many cases, Llama is sufficient, and for fine-tuning, you might want to use GPT.
+builder.Services.AddProxyForDebug()
+    .AddKernel()
+    .AddAzureOpenAIChatCompletion(deploymentGpt4, azureOpenAiEndpoint, azureOpenAiApiKey, modelId: deploymentGpt4)
+    .AddAzureOpenAIChatCompletion(deploymentGpt35, azureOpenAiEndpoint, azureOpenAiApiKey, modelId: deploymentGpt35);
 
 var app = builder.Build();
 
+// Here is the central point where the magic happens in more complex scenarios.
 app.MapPostApiChat(async (chatRequest, chatCompletionService, httpContext, kernel) =>
 {
     var chatHistory = chatRequest.ToChatHistory();
 
     var promptExecutionSettings = new OpenAIPromptExecutionSettings
     {
-        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+        ModelId = "gpt-4"
     };
 
     await chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory, promptExecutionSettings, kernel)
